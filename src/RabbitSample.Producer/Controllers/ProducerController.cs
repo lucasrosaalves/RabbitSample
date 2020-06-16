d1e8419e93018eba35efa1c808d8922a98e1c0f0
lucasrosaalves <lucasrosaalves@gmail.com>
2020-06-16 19:43:03 -0300

    add initial files

---
+++ src/RabbitSample.Producer/Controllers/ProducerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitSample.Producer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProducerController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Producer up"); 
+        }
+    }
+}
---
+++ tests/RabbitSample.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace RabbitSample.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
